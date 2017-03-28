41e3ad0ede38eb2129a9e51b21a94d26a3eadfeb
Daniel Fattal <dffattal@gmail.com>
2017-03-28 18:25:06 -0400

    added single album pages and links

--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import Songs from '../components/Songs';
 
-const Album = (props) => {
+class Album extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-  const album = props.album;
-  const currentSong = props.currentSong;
-  const isPlaying = props.isPlaying;
-  const toggleOne = props.toggleOne;
+  componentDidMount() {
+    this.props.selectAlbum(this.props.routeParams.id)
+  }
 
-  return (
-    <div className="album">
-      <div>
-        <h3>{ album.name }</h3>
-        <img src={ album.imageUrl } className="img-thumbnail" />
+  render() {
+    console.log(this)
+
+    const album = this.props.album;
+    const currentSong = this.props.currentSong;
+    const isPlaying = this.props.isPlaying;
+    const toggleOne = this.props.toggleOne;
+
+    return (
+      <div className="album">
+        <div>
+          <h3>{ album.name }</h3>
+          <img src={ album.imageUrl } className="img-thumbnail" />
+        </div>
+        <Songs
+          songs={album.songs}
+          currentSong={currentSong}
+          isPlaying={isPlaying}
+          toggleOne={toggleOne} />
       </div>
-      <Songs
-        songs={album.songs}
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        toggleOne={toggleOne} />
-    </div>
-  );
+    )
+  }
 }
 
 export default Album;
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
-import { Route, Router, hashHistory, browserHistory, Link } from 'react-router';
+import { Route, Router, hashHistory, browserHistory, Link, IndexRedirect } from 'react-router';
 import Album from './components/Album';
 import Albums from './components/Albums';
 
 
 ReactDOM.render(
-	
+
   <Router history={hashHistory}>
     <Route path='/' component={ AppContainer }>
+      <IndexRedirect to='/albums' />
     	<Route path='/albums' component={Albums} />
     	<Route path='/albums/:id' component={Album} />
     </Route>
